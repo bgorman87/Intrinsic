d1d82707480613f72d7c35ba7998896b06a6070d
Brandon Gorman <bgorman@live.com>
2024-07-26 14:49:03 -0300

    chore: Update Header component to include mobile menu functionality

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,31 +1,41 @@
+import { useState } from 'react';
 import ThemeSwitch from "./ThemeSwitch"
 import SearchBar from "./SearchBar";
 import '../styles/Header.css';
 import headerLogo from '../assets/header-logo.svg';
 
-
 interface Props {
   theme: string;
   switchTheme: () => void;
+}
 
-} 
+const Header = ({ theme, switchTheme }: Props) => {
+  const [menuOpen, setMenuOpen] = useState(false);
 
-const Header = ({theme, switchTheme}: Props) => {
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  }
+
   return (
     <header className="header">
       <div className="header__logo">
-        <img src={headerLogo}></img>
+        <img src={headerLogo} alt="Logo" />
       </div>
-      <div className="header__spacer"></div>
-      <div className="header__search">
-        <SearchBar/>
+      <div className={`header__menu ${menuOpen ? 'open' : ''}`}>
+        <div className="header__search">
+          <SearchBar />
+        </div>
+        <div className="header__theme-switch">
+          <ThemeSwitch theme={theme} switchTheme={switchTheme} />
+        </div>
       </div>
-      <div className="header__spacer"></div>
-      <div className="header__theme-switch">
-        <ThemeSwitch theme={theme} switchTheme={switchTheme}/>
+      <div className="header__burger" onClick={toggleMenu}>
+        <div className="burger-bar"></div>
+        <div className="burger-bar"></div>
+        <div className="burger-bar"></div>
       </div>
     </header>
   )
 }
 
-export default Header
+export default Header;
