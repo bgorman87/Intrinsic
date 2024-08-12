2c1ec8d120eab626fb17727a8aedcdc5c2aa6d42
Brandon Gorman <bgorman@live.com>
2024-08-11 21:41:44 -0300

    chore: Refactor calculate_quality function to improve readability and handle edge cases

--- database/schema.sql
+++ database/schema.sql
@@ -48,30 +48,27 @@ BEFORE UPDATE ON STOCKS
 FOR EACH ROW
 EXECUTE FUNCTION update_lastupdated_column();
 
--- Function to calculate quality
 CREATE OR REPLACE FUNCTION calculate_quality() RETURNS trigger AS $$
 BEGIN
-    -- Check if pe, dcf, and roe are all NULL
     IF NEW.pe IS NULL AND NEW.dcf IS NULL AND NEW.roe IS NULL THEN
-        NEW.quality := 5;
+        NEW.quality := 4;
         RETURN NEW;
     END IF;
 
-    -- Set pe, dcf, and roe to 0 if they are NULL
     NEW.pe := COALESCE(NEW.pe, 0);
     NEW.dcf := COALESCE(NEW.dcf, 0);
     NEW.roe := COALESCE(NEW.roe, 0);
 
-    -- Handle infinity values
     NEW.pe := CASE WHEN NEW.pe = 'Infinity'::float8 THEN 0 ELSE NEW.pe END;
     NEW.dcf := CASE WHEN NEW.dcf = 'Infinity'::float8 THEN 0 ELSE NEW.dcf END;
     NEW.roe := CASE WHEN NEW.roe = 'Infinity'::float8 THEN 0 ELSE NEW.roe END;
 
-    -- Calculate the number of values greater than the current value
     NEW.quality := 
         CASE
-            WHEN (NEW.pe > NEW.current) AND (NEW.dcf > NEW.current) AND (NEW.roe > NEW.current) THEN 1
-            WHEN (NEW.pe > NEW.current) AND (NEW.dcf > NEW.current) OR (NEW.pe > NEW.current) AND (NEW.roe > NEW.current) OR (NEW.dcf > NEW.current) AND (NEW.roe > NEW.current) THEN 2
+            WHEN NEW.pe > NEW.current AND NEW.dcf > NEW.current AND NEW.roe > NEW.current THEN 1
+            WHEN (NEW.pe > NEW.current AND NEW.dcf > NEW.current)
+                 OR (NEW.pe > NEW.current AND NEW.roe > NEW.current)
+                 OR (NEW.dcf > NEW.current AND NEW.roe > NEW.current) THEN 2
             WHEN (NEW.pe > NEW.current) OR (NEW.dcf > NEW.current) OR (NEW.roe > NEW.current) THEN 3
             ELSE 4
         END;
@@ -79,7 +76,6 @@ BEGIN
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
-
 
 -- Trigger to use the quality function
 CREATE TRIGGER update_quality
